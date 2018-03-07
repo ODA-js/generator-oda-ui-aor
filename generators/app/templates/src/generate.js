aba8b444a851b640861c65c24e2f0a4afffeea7a
vedanta-krit.das <vedmalex@gmail.com>
2018-03-07 16:30:12 +0300

    fix: schema path

--- generators/app/templates/src/generate.js
+++ generators/app/templates/src/generate.js
@@ -1,6 +1,7 @@
 var path = require('path');
 const { generator } = require('oda-gen-graphql');
-const schema = require('../../api-new-ui-gen/dist/schema').default;
+// const schema = require('../../api-new-ui-gen/dist/schema').default;
+const schema = require('!!<put your schema path here>!!').default;
 
 generator({
   pack: schema,
